04924bf26f694a8dc523e67f3f639d3af3dca6ca
WickTheThird <filip.bumbu2@mail.dcu.ie>
2022-11-01 10:10:24 +0000

    Swift version

---
+++ Solo Mission/Solo Mission/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  Solo Mission
+//
+//  Created by Filip Bumbu on 27/10/2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            
+            let scene = GameScene(size:CGSize(width: 1536, height: 2048))
+            scene.scaleMode = .aspectFill
+
+            // Present the scene
+            view.presentScene(scene)
+        
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
